8de4356571bf7d21640a3af402f589718ebb4daa
Felipe Caldeira <felipe.xitus@gmail.com>
2026-03-04 11:28:38 -0300

    feat: implement date validation and refactor AddTaskController

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -3,10 +3,11 @@ import { HttpRequest, HttpResponse } from "../../interfaces/http";
 import { InvalidParamError } from "../../presentations/api/errors/invalid-param-error";
 import { MissingParamError } from "../../presentations/api/errors/missing-param-error";
 import { badRequest, created } from "../../presentations/api/httpResponses/httpResponses";
-import { Request, Response } from "express";
 import { Controller } from "../../interfaces/controller";
+import { DateValidator } from "../../interfaces/dateValidator";
 
 export class AddTaskController implements Controller {
+    constructor(private readonly dateValidator: DateValidator) {}
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         const requiredFields = ["title", "description", "date"];
 
@@ -17,9 +18,7 @@ export class AddTaskController implements Controller {
         }
         const { title, description, date } = httpRequest.body;
 
-        const isValid = validator.isDate(date, {
-            format: "DD-MM-YYYY",
-        });
+        const isValid = this.dateValidator.isValid(date);
 
         if (!isValid) {
             return badRequest(new InvalidParamError("date"));
