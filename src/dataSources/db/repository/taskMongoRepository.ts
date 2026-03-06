790d9fd00386aa8a438d29717bda623869812913
Felipe Caldeira <felipe.xitus@gmail.com>
2026-03-06 08:52:32 -0300

    refactor: rename AddATaskModel to AddTaskModel and update references in TaskMongoRepository

--- src/dataSources/db/repository/taskMongoRepository.ts
+++ src/dataSources/db/repository/taskMongoRepository.ts
@@ -1,43 +1,40 @@
 import { ObjectId } from "mongodb";
 import { Task } from "../../../entities/task";
-import { AddATaskModel } from "../../../usecases/addTask";
+import { AddTaskModel } from "../../../usecases/addTask";
+import { DeleteTaskModel } from "../../../usecases/deleteTask";
 import { AddTaskRepository } from "../../../usecases/repository/addTaskRepository";
 import { DeleteTaskRepository } from "../../../usecases/repository/deleteTaskRepository";
 import { MongoManager } from "../../config/mongoManager";
 import { InvalidParamError } from "../../../adapters/presentations/api/errors/invalid-param-error";
 import { NotFoundError } from "../../../adapters/presentations/api/errors/not-found-error";
 
+
 export class TaskMongoRepository implements AddTaskRepository, DeleteTaskRepository {
-    async add(taskData: AddATaskModel): Promise<Task> {
+
+    async add(taskData: AddTaskModel): Promise<Task> {
         const taskCollection = MongoManager.getInstance().getCollection("tasks");
-        const {acknowledged, insertedId} = await taskCollection.insertOne(taskData);
-        if (!acknowledged) {
-            throw new Error("Failed to insert task");
-        }
-        const insertedTask = await taskCollection.findOne({_id: insertedId});
-        if (!insertedTask) {
-            throw new Error("Failed to retrieve inserted task");
-        }
+        const { insertedId } = await taskCollection.insertOne(taskData);
+        const taskById = await taskCollection.findOne({ _id: insertedId });
+        if (!taskById) throw new Error("Task not found");
+
         const task: Task = {
-            id: insertedTask._id.toHexString(),
-            title: insertedTask.title,
-            description: insertedTask.description,
-            date: insertedTask.date
+            id: taskById._id.toHexString(),
+            title: taskById.title,
+            description: taskById.description,
+            date: taskById.date,
         };
         return task;
     }
-    
-    async delete(taskData: { id: string }): Promise<void | Error> {
+
+    async delete(taskData: DeleteTaskModel): Promise<void | Error> {
         const taskCollection = MongoManager.getInstance().getCollection("tasks");
-        
+
         if (!ObjectId.isValid(taskData.id)) {
             return new InvalidParamError(taskData.id);
         }
         const { deletedCount } = await taskCollection.deleteOne({
             _id: new ObjectId(taskData.id),
         });
-        if (!deletedCount) {
-            return new NotFoundError("task");
-        }
+        if (!deletedCount) return new NotFoundError("task");
     }
 }
