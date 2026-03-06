736af3e28d459606fa5cd561d412d8cbda07fa42
Felipe Caldeira <felipe.xitus@gmail.com>
2026-03-06 07:15:37 -0300

    feat: implement validation for AddTaskController and refactor task controller factory

--- src/adapters/factories/taskControllerFactory.ts
+++ src/adapters/factories/taskControllerFactory.ts
@@ -1,15 +1,14 @@
-import { DateValidatorAdapter } from "../dateValidatorAdapter";
 import { AddTaskController } from "../controllers/task/addTask";
 import { DbAddTask } from "../../dataSources/db/dbAddTask";
 import { TaskMongoRepository } from "../../dataSources/db/repository/taskMongoRepository";
 import { LogErrorControllerDecorator } from "../decorators/logErrorControllerDecorator";
 import { LogErrorMongoRepository } from "../../dataSources/db/repository/logErrorMongoRepository";
+import { addTaskValidationCompositeFactory } from "./addTaskValidationCompositeFactory";
 
 export const taskControllerFactory = () => {
-    const dateValidatorAdapter = new DateValidatorAdapter();
     const taskMongoRepository = new TaskMongoRepository();
     const dbAddTask = new DbAddTask(taskMongoRepository);
-    const taskController = new AddTaskController(dbAddTask, dateValidatorAdapter);
+    const taskController = new AddTaskController(dbAddTask, addTaskValidationCompositeFactory());
     const logErrorMongoRepository = new LogErrorMongoRepository();
     return new LogErrorControllerDecorator(taskController, logErrorMongoRepository);
 };
